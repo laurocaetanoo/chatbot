24a92802c93d3cdd74c1777ebcc246a626e8399f
lauro caetanoo <laurocaetano100@hotmail.com>
2025-12-03 20:54:12 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def aplicar_estilo_responsivo():
                 font-size: 1.6rem !important;
             }
             
-            header {visibility: hidden;}
+            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             .stApp > header {display: none;}
             
