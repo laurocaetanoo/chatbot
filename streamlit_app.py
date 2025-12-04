74e5cb122edd0b2b03ec40cb23b89929236bc2ff
lauro caetanoo <laurocaetano100@hotmail.com>
2025-12-03 21:08:56 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,7 @@ MODELO_LLM_LITE = "gemini-2.5-flash-lite"
 def aplicar_estilo_responsivo():
     st.markdown("""
         <style> 
-        
-        [data-testid="stToolbar"] {
-                visibility: hidden;
-                display: none;
-        }   
+          
         #MainMenu {visibility: hidden; display: none;} /* esconde o menu hamburguer */
         [data-testid="stDecoration"] {
             display: none;
