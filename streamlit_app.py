ed01e330f9e3c44f811797070ae6cbfa8106e6c3
lauro caetanoo <laurocaetano100@hotmail.com>
2025-12-05 20:27:50 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,8 @@ def aplicar_estilo_responsivo():
 
             .block-container {
                 padding-top: 1rem !important;
-                padding-left: 0.5rem !important;
-                padding-right: 1rem !important;
+                padding-left: 1rem !important;
+                padding-right: 0.5rem !important;
                 padding-bottom: 2rem !important;
             }
             
